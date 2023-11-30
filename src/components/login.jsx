36f626eac558f778e75db483fcde0b1d18682f59
mosesshema1@gmail.com <mosesshema1@gmail.com>
2023-11-30 21:39:02 +0200

    fix

---
+++ src/components/login.jsx
@@ -0,0 +1,17 @@
+import { Box, Flex } from "@chakra-ui/react"
+
+function Login (){
+    return <>
+    <Flex>
+        <Box>
+        <FormControl>
+  <FormLabel>Email address</FormLabel>
+  <Input type='email' />
+  <FormHelperText>We'll never share your email.</FormHelperText>
+</FormControl>
+        </Box>
+    </Flex>
+    </>
+}
+
+export default Login
