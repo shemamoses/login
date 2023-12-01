5a8675059705c8deacef44ab14be1579a67560fd
mosesshema1@gmail.com <mosesshema1@gmail.com>
2023-12-01 10:52:21 +0200

    ..

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,17 +1,69 @@
-import { Box, Flex,FormLabel,FormControl } from "@chakra-ui/react"
+import { FormLabel, FormControl, Input, Flex, Button } from "@chakra-ui/react";
+import { useState } from "react";
 
-function Login (){
-    return <>
-    <Flex>
-        <Box>
-        <FormControl>
-  <FormLabel>Email address</FormLabel>
-  <Input type='email' />
-  <FormHelperText>We'll never share your email.</FormHelperText>
-</FormControl>
-        </Box>
-    </Flex>
+function Login() {
+  const [username, setUsername] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setPassword] = useState("");
+  //handling user inputs
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const handleUserEmail = (e) => {
+    setUserEmail(e.target.value);
+  };
+  const handleUserPassword = (e) => {
+    setPassword(e.target.value);
+  };
+  //handling submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, userEmail, userPassword);
+  };
+  //handling empty input fields
+
+  return (
+    <>
+      <Flex height="100vh" align="center" justify="center" padding={20}>
+        <FormControl onSubmit={handleSubmit}>
+          <FormLabel>User Name</FormLabel>
+          <Input
+            type="text"
+            id="username"
+            placeholder="Enter your username"
+            variant="filled"
+            value={username}
+            onChange={handleUsername}
+          />
+          <FormLabel>Email</FormLabel>
+          <Input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            variant="filled"
+            onChange={handleUserEmail}
+          />
+
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            variant="filled"
+            onChange={handleUserPassword}
+          />
+          <FormLabel>SUBMIT</FormLabel>
+          <Button
+            color="teal"
+            colorScheme="gray"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Login
+          </Button>
+        </FormControl>
+      </Flex>
     </>
+  );
 }
-
-export default Login
+export default Login;
